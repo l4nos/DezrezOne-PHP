1da6df0bd9df04978605939902abd1195a526d67
syn6UK <31861108+syn6UK@users.noreply.github.com>
2018-06-27 11:13:01 +0100

    Add files via upload

---
+++ drOne.php
@@ -0,0 +1,31 @@
+<?php
+
+class drOne {
+
+	public $apikey;
+	public $agencyID;
+
+	public function __construct($apiKey, $agencyID){
+			$this->apikey = $apiKey;
+			$this->agencyID = $agencyID;
+	}
+
+	public function propertySearch($criteria){
+
+	}
+
+	public function propertyDetails($id){
+
+	}
+
+	public function featuredProperties($number){
+
+	}
+
+	public function latestProperties($number){
+
+	}
+
+
+
+}
